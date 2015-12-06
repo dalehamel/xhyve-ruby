ae05a362a59f925d4ed8111dc72e8779aa5c09d7
Dale Hamel <dale.hamel@jadedpixel.com>
2015-12-06 10:32:38 -0500

    WIP

--- ext/vmnet/extconf.rb
+++
@@ -1,9 +0,0 @@
-require 'mkmf'
-require 'rbconfig'
-
-abort 'Only works on OS X' unless RbConfig::CONFIG['host_os'].downcase.include?('darwin')
-abort 'missing vmnet.h' unless have_header 'uuid.h'
-abort 'missing vmnet.h' unless have_header 'vmnet/vmnet.h'
-abort 'missing vmnet' unless have_framework 'vmnet'
-
-create_makefile 'vmnet/vmnet'
